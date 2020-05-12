bb908d1d6b3f61f7fa11815e9fd4e9a4d014ec1b
Taras Pixel <taraspixel@gmail.com>
2020-05-12 15:18:38 +0300

    changed post model in Mongoo&API

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,9 +6,10 @@ const Post = require('../models/Post')
 const router = new Router().prefix('/posts')
 
 router.post('/', passport.authenticate('jwt', { session: false }), async (ctx) => {
-  const { body } = ctx.request.body
+  const { title, body } = ctx.request.body
   const user = ctx.state.user._id
-  ctx.body = await new Post({ body, user }).save()
+  const post = await new Post({ title, body, user }).save()
+  ctx.body = { message: 'Post created successfully!', ...post._doc }
   ctx.status = 201
 })
 
@@ -19,12 +20,17 @@ router.get('/', async (ctx) => {
   delete query.limit
   const q = 'users' in query ?
     { user: { $in: query.users.split(',') } } : query
-  ctx.set('x-total-count', await Post.countDocuments(q))
-  ctx.body = await Post
+  let total = 0  
+  total = await Post.countDocuments(q)
+  const posts = await Post
     .find(q)
     .sort({ createdDate: -1 })
     .skip(+skip)
     .limit(+limit)
+  ctx.body = {
+    'posts': posts,
+    'total': total
+  }
 })
 
 router.get('/:id', async (ctx) => {
@@ -37,13 +43,14 @@ router.get('/:id', async (ctx) => {
 })
 
 router.put('/', passport.authenticate('jwt', { session: false }), async (ctx) => {
-  const { _id, body } = ctx.request.body
+  const { _id, title, body } = ctx.request.body
   const user = ctx.state.user._id
-  ctx.body = await Post.findOneAndUpdate(
+  const post = await Post.findOneAndUpdate(
     { _id, user },
-    { $set: { body } },
+    { $set: { title, body } },
     { new: true }
   )
+  ctx.body = { message: 'Post successfully updates!', ...post._doc }
 })
 
 router.delete('/:_id', passport.authenticate('jwt', { session: false }), async (ctx) => {
--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 const clc = require('cli-color')
 
 const Koa = require('koa')
+const cors = require('@koa/cors')
+
+const app = new Koa()
+
+app.use(cors())
 
 const config = require('./lib/config')
 const handlers = require('./handlers')
 const controllers = require('./controllers')
 const mongooseConfig = require('./lib/mongoose-config')
-
-const app = new Koa()
 
 handlers.forEach((h) => app.use(h))
 
